ed05e6c504bf80aa1a0764fa1604fc4b2a55665e
addday@gmail.com <addday@gmail.com>
2016-09-08 15:40:48 +0800

    79 系统主界面接口开发及联调：涉及到控制权限的菜单数据查询，还需先初始化菜单数据、角色数据、操作员与角色、角色与菜单等数据

---
+++ api/src/main/java/top/cardone/authority/func/ReadListPermissionCodeFunc.java
@@ -0,0 +1,21 @@
+package top.cardone.authority.func;
+
+import com.google.common.collect.Sets;
+import top.cardone.authority.service.PermissionService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.func.Func1;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by cardone-home-001 on 2016/4/19.
+ */
+public class ReadListPermissionCodeFunc implements Func1<Set<String>, String> {
+    @Override
+    public Set<String> func(String userCode) {
+        List<String> permissionCodeList = ApplicationContextHolder.getBean(PermissionService.class).readListPermissionCodeByUserCode(userCode);
+
+        return Sets.newHashSet(permissionCodeList);
+    }
+}
