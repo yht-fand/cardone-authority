63fd2153cdf6ef41ab70f4cbda794afd825bbe64
addday@gmail.com <addday@gmail.com>
2017-02-28 11:03:19 +0800

    update

--- implements/default/src/main/java/top/cardone/authority/service/impl/PermissionServiceImpl.java
+++ implements/default/src/main/java/top/cardone/authority/service/impl/PermissionServiceImpl.java
@@ -158,7 +158,35 @@ public class PermissionServiceImpl extends PageServiceImpl<PermissionDao> implem
     @Override
     @Transactional
     public int generateData() {
-        return this.generateData(UUID.randomUUID().toString());
+        String flagObjectCode = UUID.randomUUID().toString();
+
+        int count = this.generateData(flagObjectCode);
+
+        //用户组
+        count += ApplicationContextHolder.getBean(UserGroupService.class).generateData(flagObjectCode);
+
+        //用户组与用户
+        count += ApplicationContextHolder.getBean(UserGroupUserService.class).generateData(flagObjectCode);
+
+        //角色
+        count += ApplicationContextHolder.getBean(RoleService.class).generateData(flagObjectCode);
+
+        //用户组与角色
+        count += ApplicationContextHolder.getBean(UserGroupRoleService.class).generateData(flagObjectCode);
+
+        //用户与角色
+        count += ApplicationContextHolder.getBean(UserRoleService.class).generateData(flagObjectCode);
+
+        //角色与授权
+        count += ApplicationContextHolder.getBean(RolePermissionService.class).generateData(flagObjectCode);
+
+        //用户组与授权
+        count += ApplicationContextHolder.getBean(UserGroupPermissionService.class).generateData(flagObjectCode);
+
+        //用户与授权
+        count += ApplicationContextHolder.getBean(UserPermissionService.class).generateData(flagObjectCode);
+
+        return count;
     }
 
     @Override
