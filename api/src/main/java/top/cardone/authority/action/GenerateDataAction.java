98c03a18412ec6ab504a76096ad60787368f65cb
addday <addday@gmail.com>
2017-10-25 18:03:06 +0800

    改进：生成树结构优化 改进：生成权限优化

---
+++ api/src/main/java/top/cardone/authority/action/GenerateDataAction.java
@@ -0,0 +1,15 @@
+package top.cardone.authority.action;
+
+import top.cardone.authority.service.UserGroupService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.action.Action0;
+
+/**
+ * Created by cardo on 2017/10/25 0025.
+ */
+public class GenerateDataAction implements Action0 {
+    @Override
+    public void action() {
+        ApplicationContextHolder.getBean(UserGroupService.class).generateData();
+    }
+}
