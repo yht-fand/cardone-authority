cf9809121a08fb93f8146ad549aa456b7e3a32a0
addday <addday@gmail.com>
2018-01-16 09:12:21 +0800

    控件改进

--- api/src/main/java/top/cardone/authority/func/ReadOnePermissionsFunc.java
+++ api/src/main/java/top/cardone/authority/func/ReadOnePermissionsFunc.java
@@ -54,15 +54,15 @@ public class ReadOnePermissionsFunc implements Func1<String, Object> {
                 if (SecurityUtils.getSubject().hasRole("department-data-administrator") || SecurityUtils.getSubject().hasRole("department-tree-data-administrator")) {
                     permissionList.add((String) ApplicationContextHolder.getBean(Func1.class, this.readOneUserDepartmentCodeByUserCodeFuncBeanId).func(userCode));
                 } else {
-                    return "#";
+                    return "#permissionsEmpty#";
                 }
             } else {
-                return "#";
+                return "#permissionsEmpty#";
             }
         }
 
         if(CollectionUtils.isEmpty(permissionList)){
-            return "#";
+            return "#permissionsEmpty#";
         }
 
         if (CollectionUtils.contains(permissionList.iterator(), "*")) {
