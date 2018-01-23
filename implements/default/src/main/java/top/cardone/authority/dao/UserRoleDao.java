09dc3f0f92fa082ac1fb5321055c0c4663b8eaf2
addday <addday@gmail.com>
2018-01-23 14:31:44 +0800

    配置优化

--- implements/default/src/main/java/top/cardone/authority/dao/UserRoleDao.java
+++ implements/default/src/main/java/top/cardone/authority/dao/UserRoleDao.java
@@ -11,27 +11,27 @@ import java.util.Map;
  * @author yao hai tao
  */
 public interface UserRoleDao extends PageDao {
-	/**
-	 * 查询用户与角色对象
-	 *
-	 * @param findOne 用户与角色标识
-	 * @return 用户与角色对象
-	 */
-	Map<String, Object> findOneByUserRoleId(Map<String, Object> findOne);
+    /**
+     * 查询用户与角色对象
+     *
+     * @param findOne 用户与角色标识
+     * @return 用户与角色对象
+     */
+    Map<String, Object> findOneByUserRoleId(Map<String, Object> findOne);
 
-	/**
-	 * 生成
-	 *
-	 * @param flagObjectCode 标识对象编号
-	 * @return
-	 */
-	int generateData(String flagObjectCode);
+    /**
+     * 生成
+     *
+     * @param flagObjectCode 标识对象编号
+     * @return
+     */
+    int generateData(String flagObjectCode);
 
-	/**
-	 * 读取角色集合
-	 *
-	 * @param userCode 用户编号
-	 * @return
-	 */
-	List<String> readListRoleCodeByUserCode(String userCode);
+    /**
+     * 读取角色集合
+     *
+     * @param userCode 用户编号
+     * @return
+     */
+    List<String> readListRoleCodeByUserCode(String userCode);
 }
--- implements/default/src/main/java/top/cardone/authority/dao/impl/TokenInfoDaoImpl.java
+++ implements/default/src/main/java/top/cardone/authority/dao/impl/TokenInfoDaoImpl.java
@@ -2,7 +2,6 @@ package top.cardone.authority.dao.impl;
 
 import top.cardone.data.jdbc.dao.impl.PageDaoImpl;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,7 +13,7 @@ public class TokenInfoDaoImpl extends PageDaoImpl implements top.cardone.authori
     @Override
     public Map<String, Object> findOneByTokenInfoId(Map<String, Object> findOne) {
         String findOneSqlFilePath = this.getSqlFilePath("page.find");
-		
+
         return this.findOne(findOneSqlFilePath, findOne);
     }
 }
