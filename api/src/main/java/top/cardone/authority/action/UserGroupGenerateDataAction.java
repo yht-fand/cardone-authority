ebf6bea985e283a48da02f324ce80ddb0e690f83
addday <addday@gmail.com>
2018-02-07 08:45:34 +0800

    二级缓存优化

--- api/src/main/java/top/cardone/authority/action/UserGroupGenerateDataAction.java
+++ api/src/main/java/top/cardone/authority/action/UserGroupGenerateDataAction.java
@@ -8,7 +8,7 @@ import top.cardone.core.util.action.Action0;
 /**
  * @author cardo on 2017/10/25 0025.
  */
-public class GenerateDataAction implements Action0 {
+public class UserGroupGenerateDataAction implements Action0 {
     @Override
     public void action() {
         ApplicationContextHolder.getBean(UserGroupService.class).generateData();
