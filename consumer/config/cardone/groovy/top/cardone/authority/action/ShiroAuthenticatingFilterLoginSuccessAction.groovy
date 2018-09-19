a3ef7d81c6e5a738023a94d43d1dda137561d3d5
addday@gmail.com <addday@gmail.com>
2018-09-19 14:30:42 +0800

    超出时间段权限，需重新登录才失效，需实时失效。

--- consumer/config/cardone/groovy/top/cardone/authority/action/ShiroAuthenticatingFilterLoginSuccessAction.groovy
+++ consumer/config/cardone/groovy/top/cardone/authority/action/ShiroAuthenticatingFilterLoginSuccessAction.groovy
@@ -1,15 +1,52 @@
 package top.cardone.authority.action
 
+import org.apache.commons.lang3.ObjectUtils
 import org.apache.shiro.authc.AuthenticationToken
 import org.apache.shiro.subject.Subject
+import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.support.TaskUtils
+import top.cardone.cache.Cache
+import top.cardone.context.ApplicationContextHolder
+import top.cardone.core.util.action.Action1
 import top.cardone.core.util.action.Action4
 
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 
-class ShiroAuthenticatingFilterLoginSuccessAction implements Action4<AuthenticationToken, Subject, ServletRequest, ServletResponse>{
+class ShiroAuthenticatingFilterLoginSuccessAction implements Action4<AuthenticationToken, Subject, ServletRequest, ServletResponse> {
+    def authorityCacheTimeout
+
+    ShiroAuthenticatingFilterLoginSuccessAction() {
+        authorityCacheTimeout = ObjectUtils.defaultIfNull(ApplicationContextHolder.applicationContext.getEnvironment().getProperty("authority.cache.timeout", Long.class),
+                10 * 60 * 60)
+    }
+
     @Override
     void action(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) {
+        if (Objects.isNull(token.getPrincipal())) {
+            return
+        }
 
+        def time = ApplicationContextHolder.getBean(Cache.class).get(
+                ShiroAuthenticatingFilterLoginSuccessAction.class.getName(),
+                1,
+                token.getPrincipal(),
+                { System.currentTimeMillis() })
+
+        def timeDifference = (System.currentTimeMillis() - time) / 1000l
+
+        if (timeDifference < authorityCacheTimeout) {
+            return
+        }
+
+        ApplicationContextHolder.getBean(Cache.class).put(
+                ShiroAuthenticatingFilterLoginSuccessAction.class.getName(),
+                1,
+                token.getPrincipal(),
+                System.currentTimeMillis())
+
+        ApplicationContextHolder.getBean(TaskExecutor.class, "slowTaskExecutor").execute(TaskUtils.decorateTaskWithErrorHandler({
+            ApplicationContextHolder.getBean(Action1.class, "top/cardone/usercenter/action/EvictUserCacheAction").action(token.getPrincipal())
+        }, null, true))
     }
 }
